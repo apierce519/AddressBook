eb790bce8a199ba60a891d84b21d33c06bce99d9
cpobrien3 <cpobrien3@dmacc.edu>
2021-10-09 17:46:45 -0500

    First Commit

---
+++ src/main/java/model/AddressBook.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * @author Andrew Pierce - ajpierce1
+ * CIS175 - Fall 2021
+ * Oct 7, 2021
+ */
+public class AddressBook {
+
+	/**
+	 * 
+	 */
+	public AddressBook() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/model/ContactEntry.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * @author Christian O'Brien - cpobrien3
+ * CIS175 - Fall 2021
+ * Oct 9, 2021
+ */
+public class ContactEntry {
+	
+	private String name;
+	private LocalDate dob;
+	private String address;
+	private String phoneNumber;
+	private LocalDateTime contactCreated;
+	
+	//Constructors
+	public ContactEntry() {
+		
+	}
+	
+	public ContactEntry(String name, LocalDate dob, String address, String phoneNumber, LocalDateTime contactCreated) {
+		super();
+		this.name = name;
+		this.dob = dob;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.contactCreated = contactCreated;
+	}
+	
+	// Getters/Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public LocalDateTime getContactCreated() {
+		return contactCreated;
+	}
+	public void setContactCreated(LocalDateTime contactCreated) {
+		this.contactCreated = contactCreated;
+	}
+
+	@Override
+	public String toString() {
+		return "AddressBook [name=" + name + ", dob=" + dob + ", address=" + address + ", phoneNumber=" + phoneNumber
+				+ ", contactCreated=" + contactCreated + "]";
+	} 
+	
+	
+}
